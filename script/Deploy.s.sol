08b577e0971ca37d4b915fa50e58e7f1262c6909
XieJunhua <xiejunhua2008@gmail.com>
2024-04-26 16:09:09 +0800

    add NFTMarket deploy script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,16 +1,21 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.25 <0.9.0;
 
-import { Foo } from "../src/Foo.sol";
+import { NFTMarket, BaseERC20 } from "../src/Week2/NFTMarket.sol";
+import { MyERC721 } from "../src/Week2/MyERC721.sol";
 
 import { BaseScript } from "./Base.s.sol";
 
 /// @dev See the Solidity Scripting tutorial: https://book.getfoundry.sh/tutorials/solidity-scripting
-contract Deploy is BaseScript {
+contract NFTMarketDeploy is BaseScript {
     function run() public {
         uint256 deployPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployPrivateKey);
-        Foo foo = new Foo();
+        BaseERC20 token = new BaseERC20(vm.envAddress("ETH_FROM"));
+        MyERC721 myNFT = new MyERC721();
+
+        NFTMarket nft = new NFTMarket(address(myNFT), address(token));
+
         vm.stopBroadcast();
     }
 }
